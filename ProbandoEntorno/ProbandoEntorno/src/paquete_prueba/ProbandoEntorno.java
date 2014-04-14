f6d931f564ac128018d30a97cb75407439e78f83
Dante <dante.gs92@gmail.com>
2014-04-14 19:37:46 -0300

    Probando entorno Git

---
+++ ProbandoEntorno/ProbandoEntorno/src/paquete_prueba/ProbandoEntorno.java
@@ -0,0 +1,7 @@
+package paquete_prueba;
+
+public class ProbandoEntorno {
+public static void main(String[] args) {
+	System.out.println("hola mundo. Haciendo cambios en el proyecto para subir al server");
+}
+}
