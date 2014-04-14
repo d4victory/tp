86dbfc933c1af9900aea7b683a5f8c698d2984f9
Dante <dante.gs92@gmail.com>
2014-04-14 19:50:59 -0300

    haciendo cambios

--- ProbandoEntorno/ProbandoEntorno/src/paquete_prueba/ProbandoEntorno.java
+++ ProbandoEntorno/ProbandoEntorno/src/paquete_prueba/ProbandoEntorno.java
@@ -2,6 +2,6 @@ package paquete_prueba;
 
 public class ProbandoEntorno {
 public static void main(String[] args) {
-	System.out.println("hola mundo. Haciendo cambios en el proyecto para subir al server");
+	System.out.println("hola mundo");
 }
 }
